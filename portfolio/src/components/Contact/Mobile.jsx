7a2c2f515cdd5d86c5c374cf21a9521778dc24de
Shanmukha Reddy Vasa <shanmukhareddyvasa@gmail.com>
2025-09-14 15:12:04 +0530

    Mail modified

--- portfolio/src/components/Contact/Mobile.jsx
+++ portfolio/src/components/Contact/Mobile.jsx
@@ -23,29 +23,15 @@ function Mobile() {
         <form
           className="px-4 pt-16 pb-6 overflow-y-auto h-full font-sans text-sm text-white"
           onSubmit={(e) => {
-            e.preventDefault();
-            const mailtoLink = `mailto:shanmukhareddyvasa@gmail.com?subject=${encodeURIComponent(
-              subject
-            )}&body=${encodeURIComponent(
-              message
-            )}%0D%0A%0D%0AFrom:%20${encodeURIComponent(to)}`;
-            window.location.href = mailtoLink;
-          }}
+  e.preventDefault();
+  const mailtoLink = `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}%0D%0A%0D%0A`;
+  window.location.href = mailtoLink;
+}}
+
         >
           <h1 className="text-2xl font-medium mb-4 text-white0">New Message</h1>
 
           {/* To Field */}
-          <div className="border-b border-gray-300 py-2 flex flex-wrap items-start text-lg font-medium">
-            <label className="text-gray-200 mr-2 mb-1">From</label>
-            <input
-              type="email"
-              value={to}
-              onChange={(e) => setTo(e.target.value)}
-              className="outline-none flex-1 text-white bg-transparent min-w-[150px]"
-              placeholder="example@gmail.com"
-              required
-            />
-          </div>
 
           <div className="border-b border-gray-300 py-2 flex flex-wrap items-start text-lg font-medium">
             <label className="text-gray-200 mr-2 mb-1">Subject</label>
@@ -73,10 +59,10 @@ function Mobile() {
           <div className="absolute bottom-0 left-0 w-full border-t border-gray-300 bg-gray- p-4 flex justify-end">
             <button
               type="submit"
-              disabled={!to || !message}
+              disabled={!subject || !message}
               className={`text-white font-medium px-6 py-2 rounded-full transition duration-300 
     ${
-      to && message
+      subject && message
         ? "bg-blue-600 hover:bg-blue-700 cursor-pointer"
         : "bg-gray-400 cursor-not-allowed"
     }
